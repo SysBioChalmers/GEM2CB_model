49090728175702ecf8ada0c57d2ea607eab1239a
lhao <lhao@chalmers.se>
2019-10-26 18:49:27 +0200

    Revert "feat:"
    This reverts commit dbb437223b6f8b4ce49c4134cde98551eaffe1de.

--- ComplementaryScripts/Case_2_e_coli_core.py
+++ ComplementaryScripts/Case_2_e_coli_core.py
@@ -16,23 +16,21 @@ import GEM2pathways
 import scipy.io as sio
 import os
 import numpy as np
-from scipy.spatial import ConvexHull
-
 os.chdir('../ComplementaryData/')
 
-#%% <method EFMs > can not caculate
+#%% <method EFMs + MYA> can not caculate
 core_EFMs_z = np.genfromtxt('Case2_1_ecoli_core/e_coli_core_EFMs_standardized.csv', delimiter = ',')
 
-EFMs_all_points = core_EFMs_z.T
-EFMs_all_points = EFMs_all_points[:,1:]   # modes x reas
+EFM_all_points = core_EFMs_z.T
+EFM_all_points = EFM_all_points[:,1:]   # modes x reas
 experiment_datas = []
 
 
-#%% <method EFVs > can not caculate
+#%% <method EFVs + MYA> can not caculate
 core_EFVs_z = np.genfromtxt('Case2_1_ecoli_core/e_coli_core_EFVs_standardized.csv', delimiter = ',')
 
-EFVs_all_points = core_EFVs_z.T
-EFVs_all_points = EFVs_all_points[:,1:]   # modes x reas
+EFV_all_points = core_EFVs_z.T
+EFV_all_points = EFV_all_points[:,1:]   # modes x reas
 experiment_datas = []
 
 
@@ -46,6 +44,7 @@ FBAem_all_points = FBAem_all_points[:,1:]
 experiment_datas = []
 
 
+
 #%% <Our method>
 e_coli_core = cobra.io.read_sbml_model('../ComplementaryData/Case2_1_ecoli_core/e_coli_core.xml')
 #e_coli_core.reactions.get_by_id('EX_o2_e').bounds = (0.0,1000.0)
@@ -55,9 +54,9 @@ biomass_rea_id = 'BIOMASS_Ecoli_core_w_GAM'
 carbon_rea_id = 'EX_glc__D_e'
 yield_rea_ids = ['EX_ac_e','EX_for_e','EX_etoh_e','EX_lac__D_e','EX_succ_e',]
 
-step_of_biomass = 25
+step_of_biomass = 20
 carbon_uptake_direction = -1
-model.reactions.get_by_id(carbon_rea_id).bounds = (-10,-0.001)
+model.reactions.get_by_id(carbon_rea_id).bounds = (-10,0.001)
 
 # all modes
 yield_normalized_df = GEM2pathways.get_yield_space(model, biomass_rea_id,carbon_rea_id,yield_rea_ids,step_of_biomass,carbon_uptake_direction = carbon_uptake_direction,draw = False)
@@ -76,9 +75,7 @@ cutoff_persent = 0.99
 
 #%% <plot initial yield>: TODO not divided by carbon, so not yield
 
-# yield_rea_ids_name = yield_rea_ids
-yield_rea_ids_name = ['Acetate', 'Formate', 'Ethanol', 'Lacate', 'Succinate']
-
+yield_rea_ids_name = yield_rea_ids
 
 def trim_axs(axs, N):
     """little helper to massage the axs list to have correct length..."""
@@ -96,129 +93,36 @@ axs = trim_axs(axs, len(yield_rea_ids_name))
 for ax , exmet_reaction in zip(axs,yield_rea_ids_name):
     index = yield_rea_ids_name.index(exmet_reaction)+1
 
-    xy_EFMs = EFMs_all_points[:, [0,index]]
-    xy_EFVs = EFVs_all_points[:, [0,index]]
-    xy_FBAem = FBAem_all_points[:, [0,index]]
-    xy_our = our_all_points[:, [0,index]]
+    points1 = ax.plot(EFM_all_points[:, 0], EFM_all_points[:, index], 'x', markerfacecolor='none', color='black',
+                      alpha=0.5, label='EFM', markersize=3)
+    points4 = ax.plot(EFV_all_points[:,0], EFV_all_points[:,index],'^',markerfacecolor='none',color = 'tab:blue',
+                      alpha = 0.5,label = 'This_methd',markersize=3)
+    points3 = ax.plot(our_all_points[:, 0], our_all_points[:, index], '^', markerfacecolor='none', color='tab:red',
+                      alpha=0.8, label='EFM', markersize=5)
+    points2 = ax.plot(FBAem_all_points[:, 0], FBAem_all_points[:, index], '.', color='tab:green',
+                      alpha=1, label='FBA_mode', markersize=10)
 
-    colors_list = ['blue','teal','tab:red','tab:orange']
 
-    points_EFMs = ax.plot(xy_EFMs[:,0], xy_EFMs[:,1],       '.', markerfacecolor='none', color=colors_list[0],
-                    alpha = 0.5, markersize=3)
-    points_EFVs = ax.plot(xy_EFVs[:,0], xy_EFVs[:,1],       '.', markerfacecolor='none', color=colors_list[1],
-                    alpha = 0.5, markersize=3)
 
-    points_our = ax.plot(xy_our[:,0], xy_our[:,1],          '^', markerfacecolor='none', color=colors_list[2],
-                    alpha=0.8,  markersize=6)
-    points_FBAem = ax.plot(xy_FBAem[:,0], xy_FBAem[:,1],    's', color=colors_list[3],
-                    alpha=1,  markersize=8)
+    ax.set_ylabel(yield_rea_ids_name[index-1]+'/Glucose',fontsize = 12)
 
-    draw_hull = True
-
-    if draw_hull:
-        hull_EFMs = ConvexHull(xy_EFMs,qhull_options = qhull_options )
-        hull_EFVs = ConvexHull(xy_EFVs,qhull_options = qhull_options )
-        hull_FBAem = ConvexHull(xy_FBAem,qhull_options = qhull_options )
-        hull_our = ConvexHull(xy_our,qhull_options = qhull_options )
-
-        for simplex in hull_EFMs.simplices:
-            line_EFMs = ax.plot(xy_EFMs[simplex, 0], xy_EFMs[simplex, 1], 'x-', markerfacecolor='none', color=colors_list[0],
-                            alpha= 0.5 ,  markersize=3,lw=2)
-
-        for simplex in hull_EFVs.simplices:
-            line_EFVs = ax.plot(xy_EFVs[simplex, 0], xy_EFVs[simplex, 1], 'v-', markerfacecolor='none', color=colors_list[1],
-                            alpha = 0.5, markersize=3 ,lw=2)
-
-        for simplex in hull_our.simplices:
-            line_our = ax.plot(xy_our[simplex, 0], xy_our[simplex, 1], '^-',markerfacecolor='none', color=colors_list[2],
-                            alpha = 0.5, markersize=5,lw=2)
-
-        for simplex in hull_FBAem.simplices:
-            line_FBAem = ax.plot(xy_FBAem[simplex, 0], xy_FBAem[simplex, 1], 'o-', markerfacecolor='none', color=colors_list[3],
-                            alpha = 0.5, markersize=10,lw=8)
-
-    else:
-        line_EFMs = points_EFMs
-        line_EFVs = points_EFVs
-        line_our = points_our
-        line_FBAem = points_FBAem
-
-    ax.set_ylabel(yield_rea_ids_name[index-1]+' / Glc',fontsize = 18)
-
-ax.set_xlabel('Yield Biomass/Glucose',fontsize = 18)
-fig.legend((line_EFMs[0],line_EFVs[0],line_our[0],line_FBAem[0]),('EFMs','EFVs','This study','FBA modes'),bbox_to_anchor=(0.6, 0.3), loc='upper left', borderaxespad=0.,prop={'size': 18})
+ax.set_xlabel('Yield Biomass/Glucose',fontsize = 12)
+fig.legend((points1[0],points4[0],points2[0],points3[0]),('EFMs','EFVs','FBA modes','This study'),bbox_to_anchor=(0.55, 0.25), loc='upper left', borderaxespad=0.)
 fig.show()
 
-fig.savefig('Case2_1_ecoli_core/4.png',format ='png')
+
+# fig = plt.figure()
+# ax1 = fig.add_subplot(111)
+# index = 5
+# ax1.plot(EFM_all_points[:,0], EFM_all_points[:,index],'x',markerfacecolor='none',color = 'black',label = 'EFM',markersize=5)
+# ax1.plot(EFV_all_points[:,0], EFV_all_points[:,index],'^',markerfacecolor='none',color = 'tab:green',alpha = 0.8,label = 'FBA_mode',markersize=5)
+# ax1.plot(FBAem_all_points[:,0], FBAem_all_points[:,index],'o',markerfacecolor='none',color = 'tab:red',alpha = 0.8,label = 'FBA_mode',markersize=12)
+#
+# #ax1.plot(our_all_points[:,0], our_all_points[:,index],'v',markerfacecolor='none',color = 'tab:blue',alpha = 0.8,label = 'This_methd',markersize=12)
+# ax1.legend()
+# ax1.set_xlabel('Yield Biomass/Glucose',fontsize = 12)
+# ax1.set_ylabel(yield_rea_ids[index-1]+'/Glucose',fontsize = 12)
+# fig.show()
 
 
-# %% ac
 
-yield_rea_ids_name = ['Acetate', 'Formate', 'Ethanol', 'Lacate', 'Succinate']
-yield_rea_ids_name = ['Acetate']
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-cols = 1
-rows = len(yield_rea_ids_name) // cols + 1
-figsize = (10, 8)
-# ax= fig.subplots(111)
-
-index = 1
-
-
-xy_EFMs = EFMs_all_points[:, [0,index]]
-xy_EFVs = EFVs_all_points[:, [0,index]]
-xy_FBAem = FBAem_all_points[:, [0,index]]
-xy_our = our_all_points[:, [0,index]]
-
-colors_list = ['blue','teal','tab:red','tab:orange']
-
-points_EFMs = ax.plot(xy_EFMs[:,0], xy_EFMs[:,1],       '.', markerfacecolor='none', color=colors_list[0],
-                alpha = 0.5, markersize=3)
-points_EFVs = ax.plot(xy_EFVs[:,0], xy_EFVs[:,1],       '.', markerfacecolor='none', color=colors_list[1],
-                alpha = 0.5, markersize=3)
-
-points_our = ax.plot(xy_our[:,0], xy_our[:,1],          '^', markerfacecolor='none', color=colors_list[2],
-                alpha=0.8,  markersize=6)
-points_FBAem = ax.plot(xy_FBAem[:,0], xy_FBAem[:,1],    's', color=colors_list[3],
-                alpha=1,  markersize=8)
-
-draw_hull = True
-
-if draw_hull:
-    hull_EFMs = ConvexHull(xy_EFMs,qhull_options = qhull_options )
-    hull_EFVs = ConvexHull(xy_EFVs,qhull_options = qhull_options )
-    hull_FBAem = ConvexHull(xy_FBAem,qhull_options = qhull_options )
-    hull_our = ConvexHull(xy_our,qhull_options = qhull_options )
-
-    for simplex in hull_EFMs.simplices:
-        line_EFMs = ax.plot(xy_EFMs[simplex, 0], xy_EFMs[simplex, 1], 'x-', markerfacecolor='none', color=colors_list[0],
-                        alpha= 0.5 ,  markersize=3,lw=2)
-
-    for simplex in hull_EFVs.simplices:
-        line_EFVs = ax.plot(xy_EFVs[simplex, 0], xy_EFVs[simplex, 1], 'v-', markerfacecolor='none', color=colors_list[1],
-                        alpha = 0.5, markersize=3 ,lw=2)
-
-    for simplex in hull_our.simplices:
-        line_our = ax.plot(xy_our[simplex, 0], xy_our[simplex, 1], '^-',markerfacecolor='none', color=colors_list[2],
-                        alpha = 0.5, markersize=5,lw=2)
-
-    for simplex in hull_FBAem.simplices:
-        line_FBAem = ax.plot(xy_FBAem[simplex, 0], xy_FBAem[simplex, 1], 'o-', markerfacecolor='none', color=colors_list[3],
-                        alpha = 0.5, markersize=10,lw=2)
-
-else:
-    line_EFMs = points_EFMs
-    line_EFVs = points_EFVs
-    line_our = points_our
-    line_FBAem = points_FBAem
-
-ax.set_ylabel(yield_rea_ids_name[index-1]+' / Glc',fontsize = 18)
-
-ax.set_xlabel('Yield Biomass/Glucose',fontsize = 18)
-fig.legend((line_EFMs[0],line_EFVs[0],line_our[0],line_FBAem[0]),('EFMs','EFVs','This study','FBA modes'),bbox_to_anchor=(0.6, 0.95), loc='upper left', borderaxespad=0.,prop={'size': 16})
-fig.show()
-
-fig.savefig('Case2_1_ecoli_core/0_ac.png',format ='png')
