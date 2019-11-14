8671fa7db8a6242bd2aa1202e4b4d40a37756212
lhao <lhao@chalmers.se>
2019-11-14 13:07:18 +0100

    fix: parameter for cybernetic model

--- ComplementaryScripts/Case_1_reduced_ecoli.py
+++ ComplementaryScripts/Case_1_reduced_ecoli.py
@@ -12,6 +12,7 @@ import os
 
 import ConvexHull_yield
 import GEM2pathways
+import Cybernetic_Functions
 import cobra
 import matplotlib.pyplot as plt
 import numpy as np
@@ -206,10 +207,10 @@ fig.show()
 fig.show()
 
 # %% <Cybernetic model simulations>:
-import Cybernetic_Functions
+
 
 tStart = 0.0  # DefineTime
-tStop = 10.0
+tStop = 8.5
 tStep = 0.1
 tspan = np.linspace(tStart, tStop, (tStop - tStart) / tStep)
 
@@ -217,7 +218,9 @@ tspan = np.linspace(tStart, tStop, (tStop - tStart) / tStep)
 final_index = our_indexes[-1][-1]
 Smz = yield_normalized_df_hull_.values[:, final_index]
 Smz[0, :] = -Smz[0, :]
-
+path_for = np.array([0, 0, 0, -1, 0, 0, 0])  # Note !!! this pathway is nessary  to simulate the for experimentdata
+# Smz = np.column_stack((Smz,path_for))
+Smz = np.insert(Smz, 5, values=path_for, axis=1)
 # metabolites and pathways number
 (n_mets, n_path) = Smz.shape
 
@@ -241,25 +244,26 @@ ke = np.array([0.620342] * n_path)  # or 0.5
 # Metabolites rate Parameters kmax , Ki : dm/dt =  Smz @ V @ rM(kmax,K) * c
 # kmax : n_path
 kmax = np.array([
-    1.1279e+01,
-    2.0433e+01,
-    4.2381e-09,
-    2.1705e+00,
-    1.2650e-06,
+    1.0895e+01,
+    2.6936e+01,
+    4.1210e-09,
+    2.2697e+00,
+    8.4682e-07,
+    7.9862e+00
 ])
 
 # K : n_path
 K = np.array([
-    8.6831e-05,
-    8.8476e+00,
-    3.0966e+00,
-    3.9690e-07,
-    8.7732e-07,
-    1.9539e+02
+    1.5235e-5,
+    1.5181e+01,
+    3.5111e+00,
+    2.4034e-5,
+    6.7265e-5,
+    7.8,
 ])
 
 # carbon number for each pathways
-n_carbon = np.array([6] * n_path)
+n_carbon = np.array([6, 6, 6, 6, 6, 0])
 Biomass_index = 1
 sub_index = 0
 
@@ -290,16 +294,17 @@ def rate_def(x, CB_model):
 
     (n_mets, n_path) = Smz.shape
 
-    rM = [
-        kmax[0] * x[0 + n_mets] * x[sub_index] / (K[0] + x[sub_index]) / (1 + x[3] / K[-1]),
-        kmax[1] * x[1 + n_mets] * x[sub_index] / (K[1] + x[sub_index]) / (1 + x[3] / K[-1]),
-        kmax[2] * x[2 + n_mets] * x[sub_index] / (K[2] + x[sub_index]),
-        kmax[3] * x[3 + n_mets] * x[sub_index] / (K[3] + x[sub_index]) / (1 + x[3] / K[-1]),
-        kmax[4] * x[4 + n_mets] * x[sub_index] / (K[4] + x[sub_index]) / (1 + x[3] / K[-1]),
 
+    rM = [
+        kmax[0] * x[0 + n_mets] * x[sub_index] / (K[0] + x[sub_index]),
+        kmax[1] * x[1 + n_mets] * x[sub_index] / (K[1] + x[sub_index]),
+        kmax[2] * x[2 + n_mets] * x[sub_index] / (K[2] + x[sub_index]),
+        kmax[3] * x[3 + n_mets] * x[sub_index] / (K[3] + x[sub_index]),
+        kmax[4] * x[4 + n_mets] * x[sub_index] / (K[4] + x[sub_index]),
+        kmax[5] * (x[3] ** 2) / ((K[5] ** 2) + (x[3] ** 2)),
     ]
 
-    rE = ke * rM / kmax / x[n_mets:]
+    rE = ke * rM / kmax / np.array(list(x[n_mets:-1]) + [1])
 
     rG = Smz[Biomass_index, :] * rM[:]  # 11: biomass index
 
@@ -308,7 +313,7 @@ def rate_def(x, CB_model):
 
 sol = Cybernetic_Functions.cb_model_simulate(ecoli_reduced_our_cb, tspan, draw=False)
 
-# %% deaw
+# %% <plot cybernetic model result>
 
 # experiment data
 
