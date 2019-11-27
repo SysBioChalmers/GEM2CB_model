2537266beb9ac0cf7b745df70e8e2f31bf969b0b
lhao <lhao@chalmers.se>
2019-11-27 18:37:03 +0100

    feat: add parameters fitting function TODO: optimal 'options'(xtol=0.0001, ftol=0.0001, maxiter=None,) of fitting function

--- ComplementaryScripts/Cybernetic_Functions.py
+++ ComplementaryScripts/Cybernetic_Functions.py
@@ -10,12 +10,13 @@
 """
 import os
 
+import __main__
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.integrate import odeint
-import __main__
-
+import copy
+import matplotlib
 
 
 # %% <def functions>
@@ -98,23 +99,28 @@ def dxdy(x, t, CB_model):
 
     # rM, rE, rG = __main__.rate_def2(x, CB_model).rM , rate_def2(x, CB_model).rE , rate_def2(x, CB_model).rG
 
-    # cybernetic_var = abs(Smz[sub_index, :] * rM * n_carbon)
-    cybernetic_var = rM * n_carbon
-    # cybernetic_var[cybernetic_var==0] = 1
-    cybernetic_var[cybernetic_var < 0] = 0
-    if sum(cybernetic_var) > 0:
-        u = cybernetic_var / sum(cybernetic_var)
-        v = cybernetic_var / np.max(abs(cybernetic_var))
-    else:
-        u = v = np.zeros(n_path)
+    try:
+        u, v = __main__.cybernetic_var_def(rM, CB_model)
 
-    if CB_model.name in ['CB model for Ecoli reduced matrix ', 'CB model for Ecoli iML1515 matrix ']:
-        u[-1] = 1.0
-        v[-1] = 1.0
-        # print(CB_model.name)
-    # if CB_model.name in [ 'CB model for Ecoli core matrix '] :
-    #     u[u<0.001] = 1.0
-    #     v[v<0.001] = 1.0
+    except:
+        print('stand cybernetic_vars')
+        # cybernetic_var = abs(Smz[sub_index, :] * rM * n_carbon)
+        cybernetic_var = rM * n_carbon
+        # cybernetic_var[cybernetic_var==0] = 1
+        cybernetic_var[cybernetic_var < 0] = 0
+        if sum(cybernetic_var) > 0:
+            u = cybernetic_var / sum(cybernetic_var)
+            v = cybernetic_var / np.max(abs(cybernetic_var))
+        else:
+            u = v = np.zeros(n_path)
+
+        if CB_model.name in ['CB model for Ecoli reduced matrix ', 'CB model for Ecoli iML1515 matrix ']:
+            u[-1] = 1.0
+            v[-1] = 1.0
+            # print(CB_model.name)
+        # if CB_model.name in [ 'CB model for Ecoli core matrix '] :
+        #     u[u<0.001] = 1.0
+        #     v[v<0.001] = 1.0
 
 
     V = np.eye(n_path) * v
@@ -129,12 +135,12 @@ def dxdy(x, t, CB_model):
     dy_dx_enzyme = (mumax + beta) / (alpha + ke) * (alpha + rE * u) - (beta + mu) * x[n_mets:];
 
     dxdt_vector = list(dy_dx_mets) + list(dy_dx_enzyme)
-    if abs(t - 0.0) < 0.01:
-        print('t', t)
-        print('rM', rM)
-        print('(mumax + beta) / (alpha + ke) * (alpha + rE * u)', (mumax + beta) / (alpha + ke) * (alpha + rE * u))
-        print('(beta + mu) * x[n_mets:]', (beta + mu) * x[n_mets:])
-        print('x', x)
+    # if abs(t - 0.0) < 0.01:
+    #     print('t', t)
+    #     print('rM', rM)
+    #     print('(mumax + beta) / (alpha + ke) * (alpha + rE * u)', (mumax + beta) / (alpha + ke) * (alpha + rE * u))
+    #     print('(beta + mu) * x[n_mets:]', (beta + mu) * x[n_mets:])
+    #     print('x', x)
 
 
     return dxdt_vector
@@ -145,12 +151,13 @@ def cb_model_simulate(CB_model, tspan, draw=True):
     sol = odeint(dxdy, initial_x0, tspan, args=(CB_model,))
 
     if draw:
+        fig, ax = plt.subplots()
         for key in range(0, CB_model.Smz.shape[0]):
-            model_line = plt.plot(tspan, sol[:, key], color="k", linewidth=2)
-        plt.legend((model_line[0],), ("HCM FBA",), fontsize=18)
-        plt.xlabel("Time (hr)", fontsize=20)
-        plt.ylabel("Abundance (mM)", fontsize=20)
-        plt.show()
+            model_line = ax.plot(tspan, sol[:, key], color="k", linewidth=2)
+        ax.legend((model_line[0],), ("HCM FBA",), fontsize=18)
+        ax.set_xlabel("Time (hr)", fontsize=20)
+        ax.set_xlabel("Abundance (mM)", fontsize=20)
+        fig.show()
     return sol
 
 
@@ -275,7 +282,7 @@ if __name__ == '__main__':
         tStart = 0.0
         tStop = 15
         tStep = 0.1
-        tspan = np.linspace(tStart, tStop, (tStop - tStart) / tStep)
+        tspan = np.linspace(tStart, tStop, ((tStop - tStart) / tStep) + 1)
 
         # matrix Z: reactions x pathways; and Smz metabolites x pathways , S metabolites x reactions : Smz = Sm @ Z
         print('\n---------- Loading FBA modes ... ---------- ')
@@ -330,7 +337,7 @@ if __name__ == '__main__':
         # kmax : n_path
 
         kmax = np.array([
-            10.86,
+            10,
             35.17,
             4.1210e-09,
             2.31,
@@ -363,10 +370,25 @@ if __name__ == '__main__':
         ecoli_reduced_cb.n_carbon = n_carbon
         ecoli_reduced_cb['sub_index'] = 0
         ecoli_reduced_cb['Biomass_index'] = 1
-        CB_model = ecoli_reduced_cb
+        CB_model = copy.deepcopy(ecoli_reduced_cb)
+
+
+        # CB_model = copy.deepcopy(ecoli_reduced_cb)
+        # CB_model.kmax[[1,2,3]] = [11,22,33]
+        # CB_model.K[[1,2]] = [77,55]
+
+        # if num_kmax > 0:
+        #     index = para_to_fit['kmax']
+        #     CB_model.kmax[index] = [11,22,33]
+        # num_K = len(para_to_fit['K'])
+        # if num_K > 0:
+        #     index = para_to_fit['K']
+        #     CB_model.K[index] = [44,55]
+
 
 
         def rate_def(x, CB_model):
+            # print('def rate')
             Smz = CB_model.Smz
             kmax = CB_model.kmax
             K = CB_model.K
@@ -393,5 +415,144 @@ if __name__ == '__main__':
             rG = Smz[Biomass_index, :] * rM[:]  # 11: biomass index
 
             return rM, rE, rG
-        #
+
+
+        def cybernetic_var_def(rM, CB_model):
+            # print('def cybernetic_var')
+            (n_mets, n_path) = CB_model.Smz.shape
+            # cybernetic_var = abs(Smz[sub_index, :] * rM * n_carbon)
+            cybernetic_var = rM * CB_model.n_carbon
+            # cybernetic_var[cybernetic_var==0] = 1
+            cybernetic_var[cybernetic_var < 0] = 0
+            if sum(cybernetic_var) > 0:
+                u = cybernetic_var / sum(cybernetic_var)
+                v = cybernetic_var / np.max(abs(cybernetic_var))
+            else:
+                u = v = np.zeros(n_path)
+
+            if CB_model.name in ['CB model for Ecoli reduced matrix ', 'CB model for Ecoli iML1515 matrix ']:
+                u[-1] = 1.0
+                v[-1] = 1.0
+            return u, v
+
+    sol = cb_model_simulate(CB_model, tspan, draw=True)
+    import scipy
+
+
+    # %%
+    def update_paras_func(x_paras, _CB_model, para_to_fit, retern_model_or_paras='model'):
+        num_kmax = len(para_to_fit['kmax'])
+        num_K = len(para_to_fit['K'])
+
+        if retern_model_or_paras == 'model':
+            if num_kmax > 0:
+                _CB_model['kmax'][para_to_fit['kmax']] = x_paras[0:num_kmax]
+            if num_K > 0:
+                _CB_model['K'][para_to_fit['K']] = x_paras[num_kmax:num_kmax + num_K]
+            return _CB_model
+        else:
+            x_paras = [0] * (num_kmax + num_K)
+            if num_kmax > 0:
+                x_paras[0:num_kmax] = _CB_model['kmax'][para_to_fit['kmax']]
+            if num_K > 0:
+                x_paras[num_kmax:num_kmax + num_K] = _CB_model['K'][para_to_fit['K']]
+            # paras_initial = x_paras
+            return x_paras
+        # TODO return other para_to_fit keys
+
+
+    def residuals_func(x_paras, _CB_model, para_to_fit, exp_y, tspan, time_points_index, metas_index, weights=[],
+                       draw=False):
+
+        _CB_model = update_paras_func(x_paras, _CB_model, para_to_fit, retern_model_or_paras='model')
+
+        sol_temp = cb_model_simulate(_CB_model, tspan, draw=False)
+
+        model_y = sol_temp[time_points_index, :][:, metas_index]
+
+        if len(weights) == 0:
+            weights = 1
+
+        res = exp_y - model_y
+        res_sq = res ** 2
+        weight_of_method = 1 / np.mean(res_sq, axis=0) ** 2 / len(time_points_index)
+        weight_of_method = weight_of_method / sum(weight_of_method)
+        f = sum(res_sq * weight_of_method * weights)
+
+        # f = sum(abs(exp_y - model_y)) * weights
+        # print('f',f)
+        # print('x_paras',x_paras)
+
+        if draw:
+            global fitting_fig, fitting_ax, model_lines
+            for i in range(0, _CB_model.Smz.shape[0]):
+                model_lines[i][0].remove()
+                model_lines[i] = fitting_ax.plot(tspan, sol_temp[:, i], color="k", linewidth=2)
+                plt.pause(1e-9)
+            fitting_fig.show()
+        # return 0
+        return abs(sum(f))
+
+
+    def parameters_fitting(CB_model, experiment_data_df, para_to_fit, tspan, draw=False):
+
+        _CB_model = copy.deepcopy(CB_model)
+        # _CB_model = CB_model.copy()
+        # para_index = {'kmax': [0, 1, 2, 3, 4, 5], }
+
+        time_points = experiment_data_df.time
+        time_points_index = []
+        for time_point in time_points:
+            index = np.argmin(abs(tspan - time_point))
+            time_points_index.append(index)
+        _CB_model['metas_names'] = ['glc', 'succ', 'for', 'lac', 'ac', 'etoh', 'biomass', ]
+
+        metas_index = []
+        for exp_met_name in experiment_data_df.columns:
+            if exp_met_name in _CB_model.metas_names:
+                index = _CB_model.metas_names.index(exp_met_name)
+                metas_index.append(index)
+
+        exp_y = experiment_data_df[_CB_model.metas_names].values
+
+        sol_initial = cb_model_simulate(_CB_model, tspan, draw=False)
+
+        paras_initial = update_paras_func([], _CB_model, para_to_fit, retern_model_or_paras='paras')
+
+        # model_y = sol[timepoints_index,:][:,metas_index]
+        print('Fiting')
+        if draw:
+            global fitting_fig, fitting_ax, model_lines
+            print('drawing')
+            matplotlib.use("Qt5Agg")
+            plt.ion()
+            fitting_fig, fitting_ax = plt.subplots()
+            fitting_ax.set_xlabel("Time (hr)", fontsize=20)
+            fitting_ax.set_ylabel("Concentration (mM)", fontsize=20)
+            exp_ponts = []
+            model_lines = []
+            for i in range(0, _CB_model.Smz.shape[0]):
+                exp_ponts.append(fitting_ax.plot(experiment_data_df.time, exp_y[:, i], 'o'))
+                model_lines.append(fitting_ax.plot(tspan, sol_initial[:, i], color="k", linewidth=2))
+                plt.pause(0.01)
+                # fitting_fig.show()
+            # fitting_ax.legend((model_line[0],), ("HCM FBA",), fontsize=18)
+            # exp_ponts[1][0].remove()
+            fitting_fig.show()
+            # fitting_fig.clf()
+            # plt.close()
+
+        minimum = scipy.optimize.fmin(residuals_func, paras_initial, args=(
+            _CB_model, para_to_fit, exp_y, tspan, time_points_index, metas_index, [], draw),
+                                      xtol=0.01, ftol=0.01, maxiter=200, full_output=True)
+        print(minimum[0])
+        print(minimum[1])
+        return minimum
+
+
+    para_to_fit = {'kmax': [0, 1, 2, 3, 4, 5], 'K': [0, 1, 2, 3, 4, 5]}
+    minimum = parameters_fitting(CB_model, experiment_data_df, para_to_fit, tspan, draw=True)
+
+    # matplotlib.use("TkAgg")
+    CB_model = update_paras_func(minimum[0], CB_model, para_to_fit, retern_model_or_paras='model')
     sol = cb_model_simulate(CB_model, tspan, draw=True)
